584d4256c9c4f97881d568b620fd0fc60cadbede
akshay <akshay@aks.com>
2020-09-22 16:16:02 +0530

    First commit Grid class working

---
+++ Assets/Scripts/GameSystem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pacman
+{
+    public class GameSystem : MonoBehaviour
+    {
+        [SerializeField] private int columns, rows;
+        [SerializeField] private float cellSize;
+
+        [SerializeField] private GameObject demoPrefab;
+        [SerializeField] private Transform gridParent;
+        private Grid grid;
+        private void Start()
+        {
+            grid = new Grid(columns,rows, cellSize,this);
+        }
+        [ContextMenu("Display Grid")]
+        private void DisplayGrid()
+        {
+            grid = new Grid(columns,rows, cellSize,this);
+            var positions = grid.GetAllPositions();
+
+            for (int row = 0; row < positions.GetLength(1); row++)
+            {
+                for (int column = 0; column < positions.GetLength(0); column++)
+                {
+                    var g = GameObject.Instantiate(demoPrefab);
+                    g.transform.position = positions[column, row];
+                    g.transform.parent = gridParent;
+                }
+            }
+        }
+    }
+}
+
---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pacman
+{
+    public class Grid
+    {
+        private int coloumns, rows;
+        private float cellSize;
+        private Vector3 worldSpace;
+
+        private int[,] gridCells;
+
+        private GameSystem gameSystem;
+
+        public Grid(int coloumns, int rows, float cellSize, GameSystem gameSystem)
+        {
+            this.coloumns = coloumns;
+            this.rows = rows;
+            this.cellSize = cellSize;
+            this.worldSpace = gameSystem.transform.position;
+            this.gameSystem = gameSystem;
+
+            gridCells = new int[coloumns, rows];
+        }
+
+        public Vector3[,] GetAllPositions()
+        {
+            Vector3[,] positions = new Vector3[coloumns, rows];
+
+            Vector3 startPos = worldSpace - Vector3.right * (coloumns/2f) * cellSize + Vector3.up * (rows/2f) * cellSize;
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int column = 0; column < coloumns; column++)
+                {
+                    positions[column, row] = new Vector3(startPos.x + column * cellSize + cellSize/2, startPos.y - row * cellSize - cellSize/2,
+                        startPos.z);
+                }
+            }
+
+            return positions;
+        }
+    }
+}
